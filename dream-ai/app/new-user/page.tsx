384c5a768686dd55df116ded9cb373d311f097c2
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-05 17:28:08 -0400

    added new user code

--- dream-ai/app/new-user/page.tsx
+++ dream-ai/app/new-user/page.tsx
@@ -1,5 +1,33 @@
-const NewUser = () => {
-    return <div>hi</div>
+import { prisma } from "@/utils/db"
+import { currentUser } from "@clerk/nextjs"
+import { User } from "@clerk/nextjs/server";
+import { create } from "domain";
+import { redirect } from "next/navigation";
+
+const createNewUser = async () => {
+    const user = await currentUser();
+
+    const match = await prisma.user.findUnique({
+        where: {
+            clerkId: user.id as string,
+        },
+    })
+
+    if (!match) {
+        const newUser = await prisma.user.create({
+            data: {
+                clerkId: user.id,
+                email: user?.emailAddresses[0].emailAddress,
+            }
+        })
+    }
+
+    redirect("/journal");
+}
+
+const NewUser = async () => {
+    await createNewUser();
+    return <div>...loading</div>
 }
 
 export default NewUser
