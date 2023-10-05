c75399544dba5c93e399318f7854a5b24a43e578
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-05 13:21:35 -0400

    finished landing page

--- dream-ai/app/page.tsx
+++ dream-ai/app/page.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link"
+
 export default function Home() {
-  return <div> Home </div>
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white"> 
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">Dream Journal</h1>
+        <p className="text-2xl text-white/60 mb-4">Time to explore our dreams!</p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">get started</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
