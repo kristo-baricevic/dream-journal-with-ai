b7326d283711c379ad3d3c01b033a0806dd38861
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-06 10:22:43 -0400

    auth

--- dream-ai/app/(dashboard)/journal/page.tsx
+++ dream-ai/app/(dashboard)/journal/page.tsx
@@ -1,12 +1,25 @@
+import { getUserByClerkID } from "@/utils/auth";
 import { prisma } from "@/utils/db";
 
 
 
 const getEntries = async () => {
-    
+    const user = await getUserByClerkID();
+    const entries = await prisma.journalEntry.findMany({
+        where: {
+            userId: user.id,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        },
+    })
+
+    return entries;
 }
 
-const JournalPage = () => {
+const JournalPage = async () => {
+    const entries = await getEntries();
+    console.log('entries', entries);
     return <div> journal </div>
 }
 
