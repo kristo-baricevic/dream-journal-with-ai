ddaf6aeec1ef3336573302fec08ff1e4981a7982
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-06 14:48:21 -0400

    sidebar

--- dream-ai/app/(dashboard)/journal/[id]/page.tsx
+++ dream-ai/app/(dashboard)/journal/[id]/page.tsx
@@ -18,10 +18,23 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => { 
     const entry = await getEntry(params.id);
+    const analysisData = [
+        {name: 'Summary', value: ""},
+        {name: 'Subject', value: ""},
+        {name: 'Mood', value: ""},
+        {name: 'Negative', value: false},
+    ]
 
     return (
-        <div className="h-full w-full p-8">
+        <div className="h-full w-full grid grid-cols-3">
+            <div className="col-span-2">
             <Editor entry={entry} />
+            </div>
+            <div className="border-l border-black/10">
+                <div className="bg-blue-200 px-6 py-10">
+                    <h2 className="text-2xl">Analysis</h2>
+                </div>
+            </div>
         </div>
     )
 }
