ee650929f9d0d1fe7c4b7094b0b4c7b959dec2e1
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-06 18:06:53 -0400

    ai parser

--- dream-ai/utils/ai.ts
+++ dream-ai/utils/ai.ts
@@ -1,4 +1,31 @@
 import { OpenAI } from "langchain/llms/openai";
+import z from 'zod';
+import { StructuredOutputParser } from 'langchain/output_parsers';
+
+const parser = StructuredOutputParser.fromZodSchema(
+    z.object({
+        mood: z
+            .string()
+            .describe('the mood of the person who wrote the journal entry.'),
+        summary: z.string().describe('quick summary of the entire entry.'),
+        negative: z
+            .boolean()
+            .describe(
+            'is the journal entry negative? (i.e. does it contain negative emotions?).'
+            ),
+        subject: z.string().describe('the subject of the journal entry.'), 
+        color: z
+            .string()
+            .describe(
+            'a hexidecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness.'
+            ),
+        sentimentScore: z
+            .number()
+            .describe(
+            'sentiment of the text and rated on a scale from -10 to 10, where -10 is extremely negative, 0 is neutral, and 10 is extremely positive.'
+            ),
+    })
+  )
 
 export const analyze = async (prompt) => {
     const model = new OpenAI({temperature: 0, modelName: 'gpt-3.5-turbo'});
