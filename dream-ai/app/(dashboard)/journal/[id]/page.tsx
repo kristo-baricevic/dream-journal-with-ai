f15bdf47cc4c82a3c4c402321fa19bea4a0746a5
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-07 12:38:02 -0400

    changed destructuring

--- dream-ai/app/(dashboard)/journal/[id]/page.tsx
+++ dream-ai/app/(dashboard)/journal/[id]/page.tsx
@@ -22,36 +22,11 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => { 
     const entry = await getEntry(params.id);
-    const {mood, summary, color, subject, negative} = entry?.analysis;
-    const analysisData = [
-        {name: 'Summary', value: summary},
-        {name: 'Subject', value: subject},
-        {name: 'Mood', value: mood},
-        {name: 'Negative', value: color},
-    ]
 
     return (
-        <div className="h-full w-full grid grid-cols-3">
-            <div className="col-span-2">
+        <div className="h-full w-full">
+            <div>
             <Editor entry={entry} />
-            </div>
-            <div className="border-l border-black/10">
-                <div className="px-6 py-10" style={{backgroundColor: color}}>
-                    <h2 className="text-2xl">Analysis</h2>
-                </div>
-                <div>
-                    <ul>
-                        {analysisData.map((item) => (
-                            <li 
-                                key={item.name}
-                                className="flex items-center justify-between border-b border-t border-black/10"
-                            >
-                                <span className="text-lg font-semibold px-2 py-4">{item.name}</span>
-                                <span>{item.value}</span>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
             </div>
         </div>
     )
