e742b941dfce98c055ad4cafc9a012476c31f631
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-07 13:24:45 -0400

    added qa

--- dream-ai/utils/ai.ts
+++ dream-ai/utils/ai.ts
@@ -1,8 +1,16 @@
-import { OpenAI } from "langchain/llms/openai";
-import z from 'zod';
-import { StructuredOutputParser } from 'langchain/output_parsers';
-import { Propmts } from 'langcahin/prompts';
+import { OpenAI } from 'langchain/llms/openai'
 import { PromptTemplate } from 'langchain/prompts'
+import { loadQARefineChain } from 'langchain/chains'
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+
+import {
+  StructuredOutputParser,
+  OutputFixingParser,
+} from 'langchain/output_parsers'
+import { Document } from 'langchain/document'
+import { z } from 'zod'
+
 
 
 const parser = StructuredOutputParser.fromZodSchema(
@@ -53,3 +61,24 @@ export const analyze = async (content) => {
         console.log(e);
     }
 }
+
+export const qa = async (question, entries) => {
+    const docs = entries.map(
+      (entry) =>
+        new Document({
+          pageContent: entry.content,
+          metadata: { source: entry.id, date: entry.createdAt },
+        })
+    )
+    const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' })
+    const chain = loadQARefineChain(model)
+    const embeddings = new OpenAIEmbeddings()
+    const store = await MemoryVectorStore.fromDocuments(docs, embeddings)
+    const relevantDocs = await store.similaritySearch(question)
+    const res = await chain.call({
+      input_documents: relevantDocs,
+      question,
+    })
+  
+    return res.output_text
+  }
