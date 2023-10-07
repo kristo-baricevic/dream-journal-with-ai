560393ead7ecf0f6dea441019eaaa38a8b369c2b
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-07 14:46:40 -0400

    added chart

--- dream-ai/app/(dashboard)/history/page.tsx
+++ dream-ai/app/(dashboard)/history/page.tsx
@@ -1,3 +1,4 @@
+import HistoryChart from "@/components/HistoryChart";
 import { getUserByClerkID } from "@/utils/auth";
 import { prisma } from "@/utils/db";
 
@@ -7,9 +8,9 @@ const getData = async () => {
         where: {
             userId: user.id,
         },
-        select: {
-            sentimentScore: true,
-        },
+        orderBy: {
+            createdAt: "asc",
+        }
     })
 
     const sum = analyses.reduce((all, current) => all + current.sentimentScore, 0);
@@ -20,7 +21,14 @@ const getData = async () => {
 const History = async () => {
     const {avg, analyses} = await getData();
     console.log(analyses);
-    return <div>history: {avg}</div>
+    return (
+        <div className="w-full h-full">
+            <div>{`Avg. Sentiment ${avg}`}</div>
+            <div className="w-full h-full">
+                <HistoryChart data={analyses} />
+            </div>
+        </div>
+    )
 };
 
 export default History;
