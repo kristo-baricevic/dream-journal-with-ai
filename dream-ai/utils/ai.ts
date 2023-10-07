c5b645281e5ac1af522208f0ccbfc872b419ff33
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-07 11:16:19 -0400

    changed prompt

--- dream-ai/utils/ai.ts
+++ dream-ai/utils/ai.ts
@@ -1,6 +1,9 @@
 import { OpenAI } from "langchain/llms/openai";
 import z from 'zod';
 import { StructuredOutputParser } from 'langchain/output_parsers';
+import { Propmts } from 'langcahin/prompts';
+import { PromptTemplate } from 'langchain/prompts'
+
 
 const parser = StructuredOutputParser.fromZodSchema(
     z.object({
@@ -27,8 +30,27 @@ const parser = StructuredOutputParser.fromZodSchema(
     })
   )
 
-export const analyze = async (prompt) => {
+const getPrompt = (content) => {
+    const format_instructions = parser.getFormatInstructions();
+
+    const prompt = new PromptTemplate({
+        template:       
+        'Analyze the following journal entry. Follow the instructions and format your response to match the format instructions, no matter what! \n{format_instructions}\n{entry}',
+        inputVariables: ['entry'],
+        partialVariables: { format_instructions },
+    }
+
+    const input = await prompt.format({
+        entry: content,
+    })
+
+    console.log(input);
+    return input;
+}
+
+export const analyze = async (content) => {
+    const input = await getPrompt(content);
     const model = new OpenAI({temperature: 0, modelName: 'gpt-3.5-turbo'});
-    const result = await model.call(prompt);
+    const result = await model.call(input);
     console.log(result);
 }
