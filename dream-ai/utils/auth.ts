ce6bab37a60669dc84c355f864af8c56c01be928
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-07 17:42:32 -0400

    update

--- dream-ai/utils/auth.ts
+++ dream-ai/utils/auth.ts
@@ -1,4 +1,3 @@
-import type { User } from '@clerk/nextjs/api'
 import { auth } from "@clerk/nextjs"
 import { prisma } from "./db"
 
@@ -14,29 +13,3 @@ export const getUserByClerkID = async (select = {id: true}) => {
 
     return user;
 }
-
-export const syncNewUser = async (clerkUser: User) => {
-    const existingUser = await prisma.user.findUnique({
-      where: {
-        clerkId: clerkUser.id,
-      },
-    })
-  
-    if (!existingUser) {
-      const email = clerkUser.emailAddresses[0].emailAddress
-      // const { subscriptionId, customerId } = await createAndSubNewCustomer(email)
-  
-      await prisma.user.create({
-        data: {
-          clerkId: clerkUser.id,
-          email,
-          account: {
-            create: {
-              // stripeCustomerId: customerId,
-              // stripeSubscriptionId: subscriptionId,
-            },
-          },
-        },
-      })
-    }
-  }
