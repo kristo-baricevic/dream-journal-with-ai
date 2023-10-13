f12d91345657c604ba696cd003f9beebfbcf626a
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-13 11:46:36 -0400

    changed mw

--- dream-ai/middleware.ts
+++ dream-ai/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ['/', '/new-user', "/journal"],
+    publicRoutes: ['/'],
     debug: true,
 });
 
