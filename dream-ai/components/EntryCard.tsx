0cd7f4c472d555c3c22dff0679b482cb404293ec
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-15 13:16:39 -0400

    dynamic card values

--- dream-ai/components/EntryCard.tsx
+++ dream-ai/components/EntryCard.tsx
@@ -1,12 +1,20 @@
+'use client';
+
+import { useState } from "react";
+
 const EntryCard = ({entry}) => {
     const date = new Date(entry.createdAt).toDateString();
+    const [analysis, setAnalysis] = useState(entry.analysis || {});
+
+    const {mood, summary, color, subject, negative} = analysis;
+    console.log(entry);
+
     
     return (
-    <div className="divide-y divide-gray-200 overflow-hidden bg-white shadow">
-        <div className="px-4 py-5 sm:px-6">{date}</div>
-        <div className="px-4 py-5 sm:p-6">summary</div>
-        <div className="px-4 py-5 sm:px-6">mood</div>
-    </div>
+        <div className="divide-y divide-gray-200 overflow-hidden bg-white shadow">
+            <div className="px-4 py-5 sm:px-6">{date}</div>
+            <div className="px-4 py-5 sm:p-6 content-truncate">{entry.content}</div>
+        </div>
     )
 }
 
