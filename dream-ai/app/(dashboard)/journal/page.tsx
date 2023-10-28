c7f17847270d8a31a6cffade072978b6b3feb458
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-28 11:01:31 -0400

    formatting

--- dream-ai/app/(dashboard)/journal/page.tsx
+++ dream-ai/app/(dashboard)/journal/page.tsx
@@ -29,26 +29,28 @@ const JournalPage = async () => {
 
     return (
         <div className="px-10">
-          <h3 className="text-3xl mb-4 font-seri fade-in">It's Your Dreams That Make You Feel Free, Dream Baby Dream</h3>
-          <div className="mb-4">
-            <Question />
-          </div>
-          <div className="mt-20 flex justify-center">
-            <NewEntryCard />
-          </div>
-          <div className="flex justify-center">
+            <h3 className="text-3xl mb-4 font-seri fade-in">
+                It's Your Dreams That Make You Feel Free, Dream Baby Dream
+            </h3>
+            <div className="mb-4">
+                <Question />
+            </div>
+            <div className="mt-20 flex justify-center">
+                <NewEntryCard />
+            </div>
+            <div className="flex justify-center">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                     {entries.map((entry) => (
-                    <Link href={`/journal/${entry.id}`} key={entry.id}>
-                        <div>
-                            <EntryCard key={entry.id} entry={entry} />
-                        </div>
-                    </Link>
+                        <Link href={`/journal/${entry.id}`} key={entry.id}>
+                            <div>
+                                <EntryCard key={entry.id} entry={entry} />
+                            </div>
+                        </Link>
                     ))}
                 </div>
             </div>
         </div>
-      );
+    );
 }
 
 export default JournalPage;
