1308e14b86924cdc516f5ddae481fa0e81f1caac
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-29 21:44:25 -0400

    delete started

--- dream-ai/app/layout.tsx
+++ dream-ai/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
--- dream-ai/components/EntryCard.tsx
+++ dream-ai/components/EntryCard.tsx
@@ -1,11 +1,29 @@
 'use client';
 
 import { lightenColor } from "@/services/colorUtilities";
+import { deleteEntry } from "@/utils/api";
 import { JournalEntry } from "@prisma/client";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 
 const EntryCard = ({entry} ) => {
+    const router = useRouter();
+
+
+    const handleDelete = async () => {
+        
+        try {
+            // Call delete with the entry ID to delete it
+            const res = await deleteEntry(entry.id);
+            console.log("delete response", res);
+            router.push('/');
+        } catch (error) {
+            console.error("Failed to delete entry:", error);
+        }
+    };
+
+
     
     const date = new Date(entry.createdAt).toDateString();
     const [analysis, setAnalysis] = useState(entry.analysis || {});
@@ -25,6 +43,15 @@ const EntryCard = ({entry} ) => {
                     {analysis?.subject}
                     </div>
                 <div className="px-4 content-truncate z-10 font-serif">{analysis?.summary}</div>
+                    <button 
+                        onClick={handleDelete}
+                        type="submit" 
+                        className="rounded-full text-sm ml-5 shadow-xl transition duration-300 ease-in-out hover:bg-white hover:text-white"
+                    >
+                    <div>
+                        <img src="https://icons.iconarchive.com/icons/pictogrammers/material/128/trash-can-outline-icon.png" width="24" height="24" />
+                    </div>
+                </button>
             </div>             
         </div>
     )
