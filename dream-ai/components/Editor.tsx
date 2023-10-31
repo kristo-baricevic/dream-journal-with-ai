a1e41e6ff8df163d572c34274a18b33ef327c1a0
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-30 22:43:25 -0400

    basic flexbox

--- dream-ai/components/Editor.tsx
+++ dream-ai/components/Editor.tsx
@@ -24,8 +24,8 @@ const Editor = ({ entry }) => {
         {name: 'Summary', value: summary},
         {name: 'Subject', value: subject},
         {name: 'Mood', value: mood}, 
-        {name: 'Analysis', value: interpretation}, 
         {name: 'Negative', value: negative ? 'True' : 'False'},
+        {name: 'Analysis', value: interpretation}, 
     ]
     
     // useAutosave({
@@ -81,53 +81,62 @@ const Editor = ({ entry }) => {
 
     return (
         <div className="px-4 py-4">
-            <div className="w-full h-full grid grid-cols-3">
-                <div className="col-span-2 px-4">
+            <div className="flex flex-col">
+                <div className="col-span-3 px-4">
                     <div className="py-4">
                         <form>
-                            <button 
-                                onClick={handleSubmit}
-                                type="submit" 
-                                className="bg-pink-400 px-4 py-2 rounded-2xl text-lg ml-5 shadow-xl border-solid border-2 border-black transition duration-300 ease-in-out hover:bg-pink-500 hover:text-white"
+                            <div className="flex flex-row justify-center items-center">
+                            <div className="py-2 px-2">
+                                <button 
+                                    onClick={handleSubmit}
+                                    type="submit" 
+                                    className="bg-pink-400 px-4 py-2 rounded-2xl text-md ml-5 shadow-xl border-solid border-2 border-black transition duration-300 ease-in-out hover:bg-pink-500 hover:text-white"
+                                    >
+                                        Generate 
+                                        <br />a Dream!
+                                </button>
+                            </div>
+                            <div className="py-2 px-2">
+                                <button 
+                                    disabled={isLoading}
+                                    onClick={handleSave}
+                                    type="button"  
+                                    className="bg-green-400 px-4 py-2 rounded-2xl text-md ml-5 shadow-xl border-solid border-2 border-black transition duration-300 ease-in-out hover:bg-green-500 hover:text-white"
                                 >
-                                    Generate a Dream!
-                            </button>
-                            <button 
-                                disabled={isLoading}
-                                onClick={handleSave}
-                                type="button"  
-                                className="bg-green-400 px-4 py-2 rounded-2xl text-lg ml-5 shadow-xl border-solid border-2 border-black transition duration-300 ease-in-out hover:bg-green-500 hover:text-white"
-                            >
-                                Save
-                            </button>
+                                    Save
+                                </button>
+                            </div>
+                            </div>
                         </form>      
                     </div>
                     {isLoading && <div>...loading</div>}
-                    <div className="h-5/6">
+                    <div className="flex flew-grow h-96">
                         <textarea 
-                            className="w-full h-full px-4 py-4 text-xl resize-none border-solid border-2 border-black/60 rounded-lg shadow-md" 
+                            className="w-full h-full min-h-full px-4 py-4 text-xl resize-none border-solid border-2 border-black/60 rounded-lg shadow-md" 
                             value={value} 
                             onChange={(e) => setValue(e.target.value)} 
                         />
                     </div>
                 </div>
                 
-                <div className="mt-20 max-h-5/6 overflow-scroll">
-                    <div className="py-10 shadow-lg border-solid border-2 border-black/60 rounded-full" style={{backgroundColor: color}}>
-                        <h2 className="text-lg text-center">Color Analysis</h2>
-                    </div>
-                    <div className="max-h-96">
-                        <div className="mt-10 h-100 overflow-scroll">
-                            <ul className="flex flex-col px-4 py-4 gap-4">
-                                {analysisData.map((item) => (
-                                    <li 
-                                        key={item.name}
-                                        className="flex flex-col items-center justify-between shadow-lg bg-slate-100 px-4 py-2 rounded-lg border-solid border-2 border-black/60"
-                                    >
-                                        <span className="flex text-lg font-semibold px-2 py-2">{item.name}</span>
-                                        <span className="py-2 font-serif max-h-72 overflow-scroll">{item.value}</span>
+                <div className="mt-20 flex">
+                    <div className="flex">
+                        <div className="flex">
+                            <ul className="flex flex-wrap items-center justify-center px-4 py-4 gap-4">
+                                    <li>
+                                        <div className="py-12 shadow-lg border-solid border-2 border-black/60 rounded-full" style={{backgroundColor: color}}>
+                                            <h2 className="px-3 text-sm text-center">Color Analysis</h2>
+                                        </div>
                                     </li>
-                                ))}
+                                    {analysisData.map((item) => (
+                                        <li 
+                                            key={item.name}
+                                            className="flex flex-col items-center justify-between shadow-lg bg-slate-100 px-4 py-2 rounded-lg border-solid border-2 border-black/60"
+                                        >
+                                            <span className="flex text-lg font-semibold px-2 py-2">{item.name}</span>
+                                            <span className="py-2 font-serif max-h-72 overflow-scroll">{item.value}</span>
+                                        </li>
+                                    ))}
                             </ul>
                         </div>
                     </div>
