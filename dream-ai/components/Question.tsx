89cbd1a9e307252b5ada8391f207b738970f7dcf
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-31 16:42:23 -0400

    updated question

--- dream-ai/components/Question.tsx
+++ dream-ai/components/Question.tsx
@@ -7,6 +7,7 @@ const Question = () => {
     const [value, setValue] = useState("Based off of what my dreams have in common, what light phrase of silly encouragement can you give to me? Do not start your response by referencing this response or any previous responses. Begin by starting the analysis, saying the phrase in between these delineators: *** Hello dreamer. Keep dreaming. Your chunk of wisdom from the dreamworld today is: *** and then end with a joke. If there are no entries, start with a salutation and an invitation to explore the dream world together.");
     const [loading, setLoading] = useState(false);
     const [response, setResponse] = useState("");
+    const [isQuestion, setIsQuestion] = useState(false);
 
     const onChange = (e) => {
         setValue(e.target.value);
@@ -23,6 +24,26 @@ const Question = () => {
         setValue("");
         setLoading(false);
     };
+
+    const handleAskQuestion = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setIsQuestion(!isQuestion);
+        console.log("testing boolean", isQuestion);
+        setLoading(false);
+    };
+
+    const handleSubmitQuestion = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        console.log("about to askQuestion");
+        const answer = await askQuestion(value);
+        console.log("answer is here", answer);
+        setResponse(answer);
+        setValue("");
+        setLoading(false);
+    };
+
 
     return (
         <div className="flex flex-col py-4 justify-center align-middle">
@@ -48,26 +69,39 @@ const Question = () => {
                         </div>
                     </form>
                 </div>
-                <div className="flex flex-wrap px-2 py-2">
-                    <form onSubmit={handleSubmit}>
+                <div className="flex flex-col px-2 py-2">
+                    <form onSubmit={handleAskQuestion}>
                         <div className="flex px-2">
-                        {/* <input
-                            disabled={loading}
-                            onChange={onChange}
-                            value={value}
-                            type="text" 
-                            placeholder="ask a question" 
-                            className="border border-black/20 px-4 py-2 text-lg rounded-lg shadow-lg"
-                        /> */}
-                        <button 
-                            disabled={loading}
-                            type="submit" 
-                            className="bg-purple-400 px-4 py-2 rounded-2xl text-lg ml-5 shadow-xl border-solid border-2 border-black transition duration-300 ease-in-out hover:bg-purple-500 hover:text-white"
-                            >
-                            Ask a question!
-                        </button>
+                            <button 
+                                disabled={loading}
+                                type="submit" 
+                                className="bg-purple-400 px-4 py-2 rounded-2xl text-lg ml-5 shadow-xl border-solid border-2 border-black transition duration-300 ease-in-out hover:bg-purple-500 hover:text-white"
+                                >
+                                Ask a question!
+                            </button>
                         </div>
                     </form>
+                </div>
+                <div className="flex flex-row py-2 align-middle justify-center">
+                    {isQuestion && (
+                        <form onSubmit={handleSubmitQuestion}>
+                            <input
+                                disabled={loading}
+                                onChange={onChange}
+                                value={value}
+                                type="text" 
+                                placeholder="ask a question" 
+                                className="border border-black/20 px-4 py-2 text-lg rounded-lg shadow-lg"
+                            /> 
+                            <button 
+                                disabled={loading}
+                                type="submit" 
+                                className="bg-purple-300 px-4 py-2 rounded-2xl text-lg ml-5 shadow-xl border-solid border-2 border-black transition duration-300 ease-in-out hover:bg-purple-500 hover:text-white"
+                                >
+                                Submit!
+                            </button>
+                        </form>
+                    )}
                 </div>
             </div>
             <div className="flex">
