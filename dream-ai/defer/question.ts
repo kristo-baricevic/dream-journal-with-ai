d9f5b6fa1895cb64117246c8e41c1a7cf335de76
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-12-06 22:34:37 -0500

    test

--- dream-ai/defer/question.ts
+++ dream-ai/defer/question.ts
@@ -16,7 +16,7 @@ export const qa = async (question: string, entries: {id: string, createdAt: Date
     );
 
     console.log("inside QA");
-    console.log("question");
+    console.log("question")
 
     const model = new OpenAI({ temperature: 0.8, modelName: 'gpt-3.5-turbo' });
     const chain = loadQARefineChain(model);
