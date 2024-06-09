122ead0f80fee2d01a5847358fcac57fe66d7a50
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2024-06-08 23:51:34 -0400

    removed commits

--- dream-ai/services/getEntries.ts
+++ dream-ai/services/getEntries.ts
@@ -3,12 +3,10 @@ import { prisma } from "./prismaQuery";
 
 export const getEntries = async () => {
     const user = await getUserByClerkID();
-    console.log("user is " + user.id);
     const entries = await prisma.journalEntry.findMany({
         where: { userId: user.id },
         orderBy: { createdAt: 'desc' },
         include: { analysis: true },
     });
-    console.log("Entries:", entries);
     return entries;
 }
